7d0af480c1d63ed784d2e044c43e2c72ed46a8f4
Maksym Zyza <mmziza88@gmail.com>
2021-04-22 21:49:36 +0300

    updete navigation

--- index.js
+++ index.js
@@ -3,85 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.end(`
-  <div>
-      <nav>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-         <li>
-          <a href='/html'>Html</a>
-        </li>
-        </li>
-         <li>
-          <a href='/css'>CSS</a>
-        </li>
-        </li>
-         <li>
-          <a href='/js'>JS</a>
-        </li>
-        <li>
-          <a href='/react'>React</a>
-        </li>
-      </ul>
-    </nav>
-
-    <h1>Home page</h1>
-
-    <h2>Front end software development tools</h2>
-
-    <img align="left" alt="JavaScript" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
-    <img align="left" alt="React" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
-    <img align="left" alt="HTML5" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
-    <img align="left" alt="CSS3" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
-
-  </div>`);
-});
-
-app.get("/html", (req, res) => {
-  res.end(`
-  <div>
-       <nav>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-         <li>
-          <a href='/html'>Html</a>
-        </li>
-        </li>
-         <li>
-          <a href='/css'>CSS</a>
-        </li>
-        </li>
-         <li>
-          <a href='/js'>JS</a>
-        </li>
-        <li>
-          <a href='/react'>React</a>
-        </li>
-      </ul>
-    </nav>
-
-    <h1>Html page</h1>
-
-    <p>
-    The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
-Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics 
-for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets. Tags such as img and input directly introduce content into the page. Other tags such as <p> 
-surround and provide information about document text and may include other tags as sub-elements. Browsers do not display the HTML tags, but use them to interpret the content of the page.
-    </p>
-
-    <img align="left" alt="HTML5" width="200px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" /> 
-  </div>`);
-});
-
-app.get("/css", (req, res) => {
-  res.end(`
-  <div>
+const navigation = `
     <nav>
       <ul>
         <li>
@@ -103,9 +25,42 @@ app.get("/css", (req, res) => {
         </li>
       </ul>
     </nav>
+`;
 
+app.get("/", (req, res) => {
+  res.end(`
+  <div>
+    ${navigation}
+    <h1>Home page</h1>
+    <h2>Front end software development tools</h2>
+    <img align="left" alt="JavaScript" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
+    <img align="left" alt="React" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
+    <img align="left" alt="HTML5" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
+    <img align="left" alt="CSS3" width="100px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
+  </div>`);
+});
+
+app.get("/html", (req, res) => {
+  res.end(`
+  <div>
+    ${navigation}
+    <h1>Html page</h1>
+    <p>
+    The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
+Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics 
+for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets. Tags such as img and input directly introduce content into the page. Other tags such as <p> 
+surround and provide information about document text and may include other tags as sub-elements. Browsers do not display the HTML tags, but use them to interpret the content of the page.
+    </p>
+    <img align="left" alt="HTML5" width="200px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" /> 
+  </div>`);
+});
+
+app.get("/css", (req, res) => {
+  res.end(`
+  <div>
+    ${navigation}
     <h1>CSS page</h1>
-
     <p>
     Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.[2]
 CSS is designed to enable the separation of presentation and content, including layout, colors, and fonts.[3] This separation can improve content accessibility, provide more flexibility and 
@@ -115,7 +70,6 @@ Separation of formatting and content also makes it feasible to present the same 
 The name cascading comes from the specified priority scheme to determine which style rule applies if more than one rule matches a particular element. This cascading priority scheme is predictable.
 The CSS specifications are maintained by the World Wide Web Consortium (W3C). Internet media type (MIME type) text/css is registered for use with CSS by RFC 2318 (March 1998). The W3C operates a free CSS validation service for CSS documents.[5]
     </p>
-
     <img align="left" alt="CSS3" width="200px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
   </div>`);
 });
@@ -123,37 +77,14 @@ The CSS specifications are maintained by the World Wide Web Consortium (W3C). In
 app.get("/js", (req, res) => {
   res.end(`
   <div>
-    <nav>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-         <li>
-          <a href='/html'>Html</a>
-        </li>
-        </li>
-         <li>
-          <a href='/css'>CSS</a>
-        </li>
-        </li>
-         <li>
-          <a href='/js'>JS</a>
-        </li>
-        <li>
-          <a href='/react'>React</a>
-        </li>
-      </ul>
-    </nav>
-
+    ${navigation}  
     <h1>JS page</h1>
-
     <p>
     JavaScript (/ˈdʒɑːvəˌskrɪpt/),[8] often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.[9] JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.
 Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web.[10] Over 97% of websites use it client-side for web page behavior,[11] often incorporating third-party libraries.[12] All major web browsers have a dedicated JavaScript engine to execute the code on the user's device.
 As a multi-paradigm language, JavaScript supports event-driven, functional, and imperative programming styles. It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM).
 The ECMAScript standard does not include any input/output (I/O), such as networking, storage, or graphics facilities. In practice, the web browser or other runtime system provides JavaScript APIs for I/O.
     </p>
-
     <img align="left" alt="JavaScript" width="200px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
   </div>`);
 });
@@ -161,36 +92,13 @@ The ECMAScript standard does not include any input/output (I/O), such as network
 app.get("/react", (req, res) => {
   res.end(`
   <div>
-    <nav>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-         <li>
-          <a href='/html'>Html</a>
-        </li>
-        </li>
-         <li>
-          <a href='/css'>CSS</a>
-        </li>
-        </li>
-         <li>
-          <a href='/js'>JS</a>
-        </li>
-        <li>
-          <a href='/react'>React</a>
-        </li>
-      </ul>
-    </nav>
-
+    ${navigation}  
     <h1>React page</h1>
-
     <p>
     React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library[3] for building user interfaces or UI components. It is maintained by Facebook and a community of individual 
     developers and companies.[4][5][6] React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, 
     so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality.[7]
     </p>
-
     <img align="left" alt="React" width="200px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
   </div>`);
 });
